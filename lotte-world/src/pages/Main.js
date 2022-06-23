bb1e395c313b46ad8beca3502d33f5cd5dccf1de
yudahyun <57222188+yudahyun@users.noreply.github.com>
2022-06-23 19:17:54 +0900

    메인  수정

--- lotte-world/src/pages/Main.js
+++ lotte-world/src/pages/Main.js
@@ -20,6 +20,7 @@ import wave from "./../assets/images/wave-bg5.png";
 import wave2 from "./../assets/images/zone-wave3.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 const MainContainer = styled.div`
   .mainSlider {
     .slick-track {
@@ -83,9 +84,7 @@ const MainContainer = styled.div`
   }
   .animationWrap {
     padding: 200px 0 0;
-    & {
-      padding: 0;
-    }
+    
     p {
       font-size: 105px;
       font-weight: bold;
@@ -105,7 +104,7 @@ const MainContainer = styled.div`
   }
   .enjoywrap {
     background: url(${enjoyBg}) no-repeat 100% 0 / cover;
-    padding: 500px 0 100px;
+    padding: 450px 0 100px;
     position: relative;
     .enjoySlide {
       padding: 50px 0 300px;
@@ -125,14 +124,19 @@ const MainContainer = styled.div`
       bottom: 0;
     }
     h2 {
+      font-size: 63px;
+      letter-spacing: -4px;
       font-weight: bold;
       text-align: center;
-      font-size: 40px;
       color: #fff;
       line-height: 1.5;
     }
     .badgesWrap {
       text-align: center;
+      display: flex;
+      justify-content: center;
+      gap: 20px;
+      padding: 20px 0 0 0;
       span {
         border: 1px solid #fff;
         padding: 20px;
@@ -146,13 +150,21 @@ const MainContainer = styled.div`
 
     .slick-slide {
       opacity: 0.75;
+      outline: none !important;
       img {
         margin: 0 auto;
         border-radius: 15px;
         transform: scale(0.7);
       }
+
       &.slick-center {
         opacity: 1;
+        .slide-description {
+          transform: scale(1);
+          top: 0;
+          padding: 20px 0;
+        }
+
         & img {
           transform: scale(1);
         }
@@ -160,10 +172,81 @@ const MainContainer = styled.div`
       .slide-description {
         text-align: center;
         color: #fff;
+        transform: scale(0.7);
+        top: -50px;
+        position: relative;
+        padding: 0;
+
         h3 {
           font-size: 30px;
           line-height: 1.5;
           font-weight: bold;
+        }
+        p {
+          line-height: 2;
+        }
+      }
+    }
+    .slick-dots {
+      z-index: 999;
+      position: absolute;
+      top: -75px;
+      bottom: auto;
+      ul {
+        li {
+          width: auto !important;
+          height: auto !important;
+          text-align: center;
+          border: 1px solid #fff;
+          padding: 15px 15px 12px 23px;
+          text-align: center;
+          color: #fff;
+          border-radius: 50px;
+          display: inline-block;
+          cursor: pointer;
+          div {
+            &:before {
+              color: #fff;
+              font-size: 18px;
+              font-weight: bold;
+            }
+          }
+          &.slick-active {
+            background-color: #fff !important;
+            div {
+              &:before {
+                color: #000 !important;
+              }
+            }
+          }
+          &:nth-child(1) {
+            div {
+              &:before {
+                content: "언더랜드 존";
+              }
+            }
+          }
+          &:nth-child(2) {
+            div {
+              &:before {
+                content: "로얄가든 존";
+              }
+            }
+          }
+          &:nth-child(3) {
+            div {
+              &:before {
+                content: "조이풀메도우 존";
+              }
+            }
+          }
+          &:nth-child(4) {
+            div {
+              &:before {
+                content: "윈더우즈 존";
+              }
+            }
+          }
         }
       }
     }
@@ -174,22 +257,118 @@ const MainContainer = styled.div`
     h1 {
       color: #fff;
       text-align: center;
-      font-size: 40px;
+      font-size: 62px;
+      padding: 50px 0 120px 0;
       font-weight: bold;
       line-height: 1.5s;
     }
     .showWrap {
       display: flex;
       justify-content: center;
+      .showImgWrap {
+        opacity: 1;
+      }
+      &.active {
+        .showImgWrap {
+          opacity: 0.5;
+
+          &.active {
+            opacity: 1;
+            transform: translate(0, -40px);
+            transition: all 0.4s;
+            p {
+              opacity: 1;
+            }
+          }
+        }
+      }
       h4 {
         font-size: 28px;
         font-weight: bold;
+        padding: 20px 0 0 0;
       }
       h4,
       p {
         color: #fff;
         text-align: center;
         line-height: 1.5;
+      }
+      p {
+        opacity: 0;
+      }
+    }
+  }
+  .noticeWrap {
+    h1 {
+      padding: 100px 0 50px 0;
+      font-size: 63px;
+      color: #000;
+      font-weight: bold;
+      text-align: center;
+    }
+    .ListBtn {
+      display: block;
+      margin: 0 auto;
+      text-align: center;
+      cursor: pointer;
+      /* height: 40px; */
+      padding: 7px 28px;
+      font-size: 21px;
+      color: #333;
+      line-height: 39px;
+      width: 200px;
+      border-radius: 40px;
+      border: 1px solid #333;
+      font-weight: 600;
+      position: relative;
+      overflow: hidden;
+
+      &:after {
+        left: -14px;
+        content: "";
+        display: block;
+        width: 300px;
+        border-radius: 50%;
+        height: 200px;
+        background: green;
+        position: absolute;
+        top: 62px;
+        -webkit-transition: all 0.4s;
+        transition: all 0.4s;
+        z-index: -1;
+      }
+      &:hover {
+        color: #fff;
+        border: 1px solid green;
+        &:after {
+          top: -40px;
+        }
+      }
+    }
+    .noticeList {
+      width: 90%;
+      margin: 50px auto 100px;
+      display: flex;
+      justify-content: center;
+      gap: 30px;
+      a {
+        * {
+          color: #000;
+        }
+        h3 {
+          font-weight: bold;
+        }
+        h4 {
+          font-weight: bold;
+          border-bottom: 1px solid;
+          line-height: 1.5;
+          font-size: 30px;
+          padding: 30px 0;
+          margin: 20px 0;
+        }
+        h5 {
+          padding: 30px 0;
+        }
       }
     }
   }
@@ -210,9 +389,32 @@ const Main = memo(() => {
 
   const enjoySettings = {
     infinite: true,
+    dots: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     centerMode: true,
+    appendDots: (dots) => (
+      <div
+        style={{
+          // backgroundColor: "#ddd",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={{
+          // width: "30px",
+          color: "rgba(0,0,0,0)",
+          // border: "1px blue solid",
+        }}
+      >
+        {i + 1}
+      </div>
+    ),
   };
   const active1 = React.useRef();
   const active2 = React.useRef();
@@ -233,7 +435,7 @@ const Main = memo(() => {
   const handleScroll = () => {
     // console.log(window.scrollY);
     // 스크롤이 Top에서 50px 이상 내려오면 true값을 useState에 넣어줌
-    if (window.scrollY >= 220) {
+    if (window.scrollY >= 300) {
       //   setScroll(true);
 
       active1.current.childNodes[0].classList =
@@ -243,8 +445,9 @@ const Main = memo(() => {
       active1.current.childNodes[2].classList =
         "active-txt animate__animated animate__fadeInRight";
     }
-    if (window.scrollY >= 600) {
-      console.log(scroll);
+    if (window.scrollY >= 1800) {
+      // console.log(scroll);
+      console.log(window.scrollY)
       active2.current.childNodes[0].classList =
         "active-txt animate__animated animate__fadeInRight";
       active2.current.childNodes[1].classList =
@@ -256,6 +459,16 @@ const Main = memo(() => {
     // 스크롤이 50px 미만일경우 false를 넣어줌
     //   setScroll(false);
     // }
+  };
+
+  const mouseHandle = (e) => {
+    e.target.parentNode.parentNode.classList.add("active");
+    e.target.parentNode.classList.add("active");
+  };
+
+  const mouseRemove = (e) => {
+    e.target.parentNode.parentNode.classList.remove("active");
+    e.target.parentNode.classList.remove("active");
   };
 
   return (
@@ -282,12 +495,12 @@ const Main = memo(() => {
         <div className="mask-bg"></div>
         <h2>신나고 즐거운 어트랙션</h2>
         <div className="badgesWrap">
-          <span>언더랜드 존</span>
-          <span>로얄가든 존</span>
-          <span>조이풀메도우 존</span>
-          <span>팅커폴스 존</span>
-          <span>레인보우스프링스 존</span>
-          <span>원더우즈 존</span>
+          {/* <button type="button">언더랜드 존</button>
+          <button type="button">로얄가든 존</button>
+          <button type="button">조이풀메도우 존</button>
+          <button type="button">팅커폴스 존</button>
+          <button type="button">레인보우스프링스 존</button>
+          <button type="button">원더우즈 존</button> */}
         </div>
         <div className="enjoySlide">
           <Slider {...enjoySettings}>
@@ -343,17 +556,32 @@ const Main = memo(() => {
       <div className="showCon">
         <h1>즐거운 공연과 퍼레이드</h1>
         <div className="showWrap">
-          <div className="showImgWrap">
+          <div
+            className="showImgWrap"
+            onMouseEnter={mouseHandle}
+            onMouseLeave={mouseRemove}
+            data-show="first-show"
+          >
             <img src={show1} alt="공연 사진" />
             <h4>로티스 매직포레스트 퍼레이드</h4>
             <p>마법에 걸린 매직 포레스트를 구한 용감한 기사 로티를 위한 축제</p>
           </div>
-          <div className="showImgWrap">
+          <div
+            className="showImgWrap"
+            onMouseEnter={mouseHandle}
+            onMouseLeave={mouseRemove}
+            data-show="second-show"
+          >
             <img src={show2} alt="공연 사진" />
             <h4>로티스 매직포레스트 퍼레이드</h4>
             <p>마법에 걸린 매직 포레스트를 구한 용감한 기사 로티를 위한 축제</p>
           </div>
-          <div className="showImgWrap">
+          <div
+            className="showImgWrap"
+            onMouseEnter={mouseHandle}
+            onMouseLeave={mouseRemove}
+            data-show="last-show"
+          >
             <img src={show3} alt="공연 사진" />
             <h4>로티스 매직포레스트 퍼레이드</h4>
             <p>마법에 걸린 매직 포레스트를 구한 용감한 기사 로티를 위한 축제</p>
@@ -362,7 +590,26 @@ const Main = memo(() => {
       </div>
       <div className="noticeWrap">
         <h1>매직포레스트의 새 소식</h1>
-        <div></div>
+        <Link className="ListBtn" to="/customer/notice-list">
+          더 많은 소식보기
+        </Link>
+        <div className="noticeList">
+          <Link to="/customer/notice-list">
+            <h3>공지</h3>
+            <h4>코로나19 예방을 위한 롯데월드 어드벤처 부산 종합안내</h4>
+            <h5>2022.02.20</h5>
+          </Link>
+          <Link to="/customer/notice-list">
+            <h3>공지</h3>
+            <h4>코로나19 예방을 위한 롯데월드 어드벤처 부산 종합안내</h4>
+            <h5>2022.02.20</h5>
+          </Link>
+          <Link to="/customer/notice-list">
+            <h3>공지</h3>
+            <h4>코로나19 예방을 위한 롯데월드 어드벤처 부산 종합안내</h4>
+            <h5>2022.02.20</h5>
+          </Link>
+        </div>
       </div>
     </MainContainer>
   );
