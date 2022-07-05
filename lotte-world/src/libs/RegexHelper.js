4c2d4afb41bea0f5e9d260d52b152f7a70f17e7e
sheeps1026 <sheeps1026@gmail.com>
2022-07-05 21:07:59 +0900

    회원가입

---
+++ lotte-world/src/libs/RegexHelper.js
@@ -0,0 +1,200 @@
+import BadRequestException from "../exceptions/BadRequestException.js";
+
+class RegexHelper {
+  // 값의 존재 여부를 검사
+  value(field, msg) {
+    const content = field.value;
+
+    if (
+      content == undefined ||
+      content == null ||
+      (typeof content == "string" && content.trim().length == 0)
+    ) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return true;
+  }
+
+  // 아이디 검사
+  idCheck(field, min, max, msg) {
+    this.value(field, msg);
+
+    const content = field.value;
+
+    if (content.trim().length > max) {
+      throw new BadRequestException(msg, field);
+    }
+
+    if (content.trim().length < min) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return this.field(field, msg, /^[a-z0-9\-_]*$/);
+  }
+
+  // 비밀번호 검사
+  pwdCheck(field, min, max, msg) {
+    this.value(field, msg);
+
+    const content = field.value;
+
+    if (content.trim().length > max) {
+      throw new BadRequestException(msg, field);
+    }
+
+    if (content.trim().length < min) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return this.field(
+      field,
+      msg,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$/
+    );
+  }
+
+  // 비밀번호 검사 (두 값이 동일한지 검사)
+  pwdCompareTo(origin, compare, msg) {
+    this.value(origin, msg);
+    this.value(compare, msg);
+
+    var src = origin.value.trim();
+    var dsc = compare.value.trim();
+
+    if (src != dsc) {
+      throw new BadRequestException(msg, origin);
+    }
+
+    return true;
+  }
+
+  // 이름 검사
+  nameCheck(field, msg) {
+    return this.field(field, msg, /^[a-zA-Zㄱ-힣0-9]*$/);
+  }
+
+  // 생년월일 검사
+  yearCheck(field, msg) {
+    return this.field(field, msg, /^[0-9]{4}$/);
+  }
+
+  // 이메일주소 형식인지 검사하기 위해 field()를 간접적으로 호출
+  emailCheck(field, msg) {
+    return this.field(
+      field,
+      msg,
+      /^([\w-]+(?:\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+    );
+  }
+
+  // 핸드폰 번호 형식과 집전화 번호 형식 둘중하나를 충족하는지 검사
+  phoneCheck(field, msg) {
+    this.value(field, msg);
+
+    const content = field.value.trim();
+    var check1 = /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/;
+    var check2 = /^\d{2,3}\d{3,4}\d{4}$/;
+
+    // 핸드폰 형식도 아니고, 집전화 형식도 아니라면
+    if (!check1.test(content) && !check2.test(content)) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return true;
+  }
+
+  // 라디오나 체크박스가 선택된 항목인지 검사
+  check(field, len, msg) {
+    const checkedItem = Array.from(field).filter((v, i) => v.checked);
+
+    if (checkedItem.length === 0) {
+      throw new BadRequestException(msg, field[0]);
+    }
+  }
+
+  // 라디오나 체크박스의 최소 선택 갯수를 제한
+  checkMin(field, msg) {
+    const checkedItem = Array.from(field).filter((v, i) => v.checked);
+
+    if (checkedItem.length < 0) {
+      throw new BadRequestException(msg, field[0]);
+    }
+  }
+
+  // 라디오나 체크박스의 최대 선택 갯수를 제한
+  checkMax(field, len, msg) {
+    const checkedItem = Array.from(field).filter((v, i) => v.checked);
+
+    if (checkedItem.length > 0) {
+      throw new BadRequestException(msg, field[0]);
+    }
+  }
+
+  // 입력값이 정규표현식을 충족하는지
+  field(field, msg, regexExpr) {
+    this.value(field, msg);
+
+    const content = field.value;
+    const str = content.trim();
+
+    if (!regexExpr.test(str)) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return true;
+  }
+
+  // 입력값이 지정된 글자수를 초과했는지 검사
+  maxLength(field, len, msg) {
+    this.value(field, msg);
+
+    const content = field.value;
+
+    if (content.trim().length > len) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return true;
+  }
+
+  // 입력값이 지정된 글자수 미만인지 검사
+  minLength(field, len, msg) {
+    this.value(field, msg);
+
+    const content = field.value;
+
+    if (content.trim().length < len) {
+      throw new BadRequestException(msg, field);
+    }
+
+    return true;
+  }
+
+  // 숫자로만 이루어 졌는지 검사하기 위해 field()를 간접적으로 호출
+  num(field, msg) {
+    return this.field(field, msg, /^[0-9]*$/);
+  }
+
+  // 영문으로만 이루어 졌는지 검사하기 위해 field()를 간접적으로 호출
+  eng(field, msg) {
+    return this.field(field, msg, /^[a-zA-Z]*$/);
+  }
+
+  // 한글로만 이루어 졌는지 검사하기 위해 field()를 간접적으로 호출
+  kor(field, msg) {
+    return this.field(field, msg, /^[ㄱ-ㅎ가-힣]*$/);
+  }
+
+  // 영문과 숫자로만 이루어 졌는지 검사하기 위해 field()를 간접적으로 호출
+  engNum(field, msg) {
+    return this.field(field, msg, /^[a-zA-Z0-9]*$/);
+  }
+
+  // 한글과 숫자로만 이루어 졌는지 검사하기 위해 field()를 간접적으로 호출
+  korNum(field, msg) {
+    return this.field(field, msg, /^[ㄱ-ㅎ가-힣0-9]*$/);
+  }
+}
+
+export default new RegexHelper();
--- lotte-world/src/pages/user/Signin.js
+++ lotte-world/src/pages/user/Signin.js
@@ -2,8 +2,12 @@
 // @description : 이름, 아이디, 비밀번호, 비밀번호 확인, 주민번호,
 //                성별, 휴대전화, 이메일 정보를 입력하여 가입
 
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import { Link } from "react-router-dom";
+import useAxios from "axios-hooks";
+
+import regexHelper from "../../libs/RegexHelper";
+
 import styled from "styled-components";
 
 const SigninContainer = styled.form`
@@ -68,43 +72,123 @@ const SigninContainer = styled.form`
 `;
 
 const Signin = memo(() => {
-  const initialValues = { name: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formError, setFormError] = useState({});
+  // 백엔드에 데이터 저장을 위한 Ajax 요청 객체 생성
+  const [{ loading }, refetch] = useAxios(
+    {
+      url: "http://localhost:3001/info",
+      method: "POST",
+    },
+    { manual: true }
+  );
 
-  const onValueChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-    // setFormValues({ ...formValues, name: value });
-    console.log(formValues);
-  };
-
-  const onValueSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    setFormError(validation(formValues));
-  };
 
-  const validation = (values) => {};
+    // 이벤트가 발생한 폼 객체
+    const current = e.target;
+
+    // 입력값에 대한 유효성 검사
+    try {
+      // 아이디 검사
+      regexHelper.value(current.id, "필수 정보입니다. (아이디)");
+      regexHelper.idCheck(
+        current.id,
+        5,
+        20,
+        "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다."
+      );
+
+      // 비밀번호 검사
+      regexHelper.value(current.pwd, "필수 정보입니다. (비밀번호)");
+      regexHelper.pwdCheck(
+        current.pwd,
+        8,
+        16,
+        "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요."
+      );
+      regexHelper.pwdCompareTo(
+        current.pwd,
+        current.pwdCheck,
+        "비밀번호가 일치하지 않습니다."
+      );
+
+      // 이름 검사
+      regexHelper.value(current.name, "필수 정보입니다. (이름)");
+      regexHelper.nameCheck(
+        current.name,
+        "한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)"
+      );
+
+      // 생년월일 검사
+      // regexHelper.yearCheck(
+      //   current.year,
+      //   "태어난 년도 4자리를 정확하게 입력하세요."
+      // );
+
+      // 이메일 검사
+      regexHelper.emailCheck(current.email, "이메일 주소를 다시 확인해주세요.");
+
+      // 휴대전화 검사
+      regexHelper.value(current.tel, "필수 정보입니다. (휴대전화)");
+      regexHelper.phoneCheck(current.tel, "형식에 맞지 않는 번호입니다.");
+    } catch (e) {
+      window.alert(e.message);
+      e.field.focus();
+      return;
+    }
+
+    let json = null;
+
+    // 입력받은 값 취득
+    const id = current.id.value; // 아이디
+    const pwd = current.pwd.value; // 비밀번호
+    const name = current.name.value; // 이름
+    const tel = current.tel.value; // 휴대전화
+    const email = current.email.value; // 이메일
+
+    (async () => {
+      try {
+        const response = await refetch({
+          data: {
+            id: id,
+            pwd: pwd,
+            name: name,
+            tel: tel,
+            email: email,
+          },
+        });
+
+        json = response.data;
+      } catch (e) {
+        console.error(e);
+        window.alert(
+          `[${e.response.status} ${e.response.statusText}\n${e.message}]`
+        );
+      }
+
+      if (json != null) {
+        window.alert("저장되었습니다.");
+      }
+    })();
+  };
 
   return (
-    <SigninContainer onSubmit={onValueSubmit}>
+    <SigninContainer onSubmit={onSubmit}>
       <h2>회원가입</h2>
       <div className="info">
-        <label htmlFor="">아이디</label>
-        <input type="text" placeholder="아이디" />
-        <label htmlFor="">비밀번호</label>
-        <input type="password" placeholder="비밀번호" />
-        <label htmlFor="" placeholder="비밀번호 확인">
+        <label htmlFor="id">아이디</label>
+        <input type="text" name="id" placeholder="아이디" />
+
+        <label htmlFor="pwd">비밀번호</label>
+        <input type="password" name="pwd" placeholder="비밀번호" />
+        <label htmlFor="pwdCheck" placeholder="비밀번호 확인">
           비밀번호 확인
         </label>
-        <input type="password" />
-        <label htmlFor="">이름</label>
-        <input
-          type="text"
-          placeholder="이름"
-          value={formValues.name}
-          onChange={onValueChange}
-        />
+        <input type="password" name="pwdCheck" />
+
+        <label htmlFor="name">이름</label>
+        <input type="text" name="name" placeholder="이름" />
+
         <div className="info-rrn">
           <label htmlFor="">주민번호</label>
           <div>
@@ -113,14 +197,15 @@ const Signin = memo(() => {
             <input type="text" />
           </div>
         </div>
-        <label htmlFor="">전화번호</label>
-        <input type="number" placeholder="전화번호" />
-        <label htmlFor="">이메일</label>
-        <input type="email" placeholder="이메일" />
+        <label htmlFor="tel">전화번호</label>
+        <input type="text" name="tel" placeholder="전화번호" />
+
+        <label htmlFor="email">이메일</label>
+        <input type="email" name="email" placeholder="이메일" />
       </div>
-      <Link to="/TicketingPage/SigninConfirm">
-        <button type="submit">확인</button>
-      </Link>
+      {/* <Link to="/TicketingPage/SigninConfirm"> */}
+      <button type="submit">확인</button>
+      {/* </Link> */}
     </SigninContainer>
   );
 });
